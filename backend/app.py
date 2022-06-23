9b3ed41d0c23666049b0b98bb687b092ff7d571b
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2022-06-23 23:29:24 +0530

    added command to run the index.html from app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,16 @@
+from distutils.log import debug
+from unicodedata import name
+import flask
 from detect_mask import *
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
