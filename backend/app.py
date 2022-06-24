d445152498959b8f4c040fd04b2142a33e4a0c69
Akash <74108261+AkashZalke@users.noreply.github.com>
2022-06-25 01:29:27 +0530

    Ajax call to backend

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from unicodedata import name
 import flask
 from pkg_resources import yield_lines
 from detect_mask import *
-from flask import Flask, render_template , Response , request
+from flask import Flask, render_template , Response , request , jsonify
 
 app = Flask(__name__)
 
@@ -13,10 +13,18 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    output = request.get_json()
-    print(output)
-    print(type(output))
-    return render_template('index.html')
+    if request.headers.get('Content-Type') == 'video/mp4':
+        # load the full request data into memory
+        # rawdata = request.get_data()
+        
+        # or use the stream 
+        rawdata = request.stream.read()
+
+        # process the data here!
+
+        return jsonify(success=True)
+    else:
+        return render_template('index.html')
 
 
 if __name__ == "__main__":
