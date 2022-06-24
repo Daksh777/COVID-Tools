8610e1149326db86aa7f93d0bfd90b1ae712ddb2
Akash <74108261+AkashZalke@users.noreply.github.com>
2022-06-25 01:27:33 +0530

    Front End
    Front End

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,21 @@
+from crypt import methods
 from distutils.log import debug
+from sre_constants import SUCCESS
 from unicodedata import name
 import flask
+from pkg_resources import yield_lines
 from detect_mask import *
-from flask import Flask, render_template
+from flask import Flask, render_template , Response , request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+
+@app.route('/',methods=['GET','POST'])
 def index():
+    output = request.get_json()
+    print(output)
+    print(type(output))
     return render_template('index.html')
 
 
