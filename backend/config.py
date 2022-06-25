72fd861e5b54f242a81c516140779aedf4c081b8
Daksh P. Jain <jaindaksh42@gmail.com>
2022-06-25 22:30:08 +0530

    Fix path

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
-prototxtPath = r"/Users/cosmos/Documents/hacking-heist/backend/models/deploy.protext"
-weightsPath = r"/Users/cosmos/Documents/hacking-heist/backend/models/res10_300x300_ssd_iter_140000.caffemodel"
+prototxtPath = r"backend/models/deploy.protext"
+weightsPath = r"backend/models/res10_300x300_ssd_iter_140000.caffemodel"
 
-model_path = "/Users/cosmos/Documents/hacking-heist/backend/models/mask_detector.model"
+model_path = "backend/models/mask_detector.model"
